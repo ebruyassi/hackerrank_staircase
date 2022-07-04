963be5cec3c5b14c9031021d5643c36d8e7e2fdf
ebruyassi <ebruyassi@gmail.com>
2022-07-04 13:36:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=(n-i);k++){
+               Console.Write(" ");
+             }
+         for(int j=1;j<=i;j++){
+              
+            Console.Write("#");
+         }
+         Console.WriteLine();
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Result.staircase(n);
+    }
+}
+
